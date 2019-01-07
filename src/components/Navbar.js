b17057dd02f2725cd610a00277a49908d5647ad8
Dijeesh Padinharethil <dijeeshpnair@gmail.com>
2019-01-08 00:12:10 +0530

    reinvent-recap blog

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/awsugkochi.png'
+import logo from '../img/awsugkochi-2.png'
 
 const Navbar = class extends React.Component {
 
