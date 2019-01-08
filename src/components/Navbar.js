4d0107bd7bc72bb6893bd25f1f2cf92ea1f4ae6e
Dijeesh Padinharethil <dijeeshpnair@gmail.com>
2019-01-09 03:01:07 +0530

    jan19 meetup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/awsugkochi-logo.png'
-import navbar-item from './custom.css'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
