a5335f759b8f6165dcd03b803935e73e956f54e5
Dijeesh Padinharethil <dijeeshpnair@gmail.com>
2019-01-09 15:47:56 +0530

    update logo size

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="awsugkochi" style={{ height: '150px' , width: '50px'  }} />
+            <img src={logo} alt="awsugkochi" style={{ height: '100px' , width: '50px'  }} />
           </figure>
         </Link>
       </div>
